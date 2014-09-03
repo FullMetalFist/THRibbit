f92178330b1ddf49f523ffaa7d36a2d7dde23de0
Michael Vilabrera <m.vilabrera@gmail.com>
2014-09-03 01:21:21 -0400

    Initial Commit

---
+++ THRibbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  THRibbit
+//
+//  Created by Michael Vilabrera on 9/3/14.
+//  Copyright (c) 2014 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ THRibbitTests/THRibbitTests.m
@@ -0,0 +1,34 @@
+//
+//  THRibbitTests.m
+//  THRibbitTests
+//
+//  Created by Michael Vilabrera on 9/3/14.
+//  Copyright (c) 2014 Giving Tree. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface THRibbitTests : XCTestCase
+
+@end
+
+@implementation THRibbitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
